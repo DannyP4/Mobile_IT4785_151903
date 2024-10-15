c9e603fe8ddcace539806ab09bf6da55d6213ddc
Long Pham <longpd1911@gmail.com>
2024-10-15 14:47:43 +0700

    first commit

--- Tuan 6/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Tuan 6/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lifecycle.R
+import com.example.calculator.R
 
 class MainActivity : AppCompatActivity() {
 
